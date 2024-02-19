3bcbf43a27be8242cf970e498fe34d1553fa08c5
KhaledTarraf <khaled23mdjkmk@gmail.com>
2024-02-19 03:03:42 +0100

    modif bdd script

--- script_bdd.sql
+++ script_bdd.sql
@@ -1,46 +1,84 @@
 -- Table des utilisateurs
-CREATE TABLE Users (
-    UserID INT PRIMARY KEY,
-    Username VARCHAR(255) UNIQUE,
-    PasswordHash VARCHAR(255),
-    Email VARCHAR(255),
+users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_type_id INT > user_types.id,
+    parse_id VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    logged_in BOOLEAN,
+    token_facebook VARCHAR(255),
+    token_twitter VARCHAR(255),
+    user_token VARCHAR(255),
+    token_expiration DATETIME
 );
 
 -- Table des parties
-CREATE TABLE Matches (
-    MatchID INT PRIMARY KEY,
-    Status VARCHAR(50),
-    -- Autres champs spécifiques à la partie
+matches (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    status VARCHAR(50) NOT NULL,
+    player_id INT > users.id
 );
 
 -- Table des invitations
-CREATE TABLE Invitations (
-    InvitationID INT PRIMARY KEY,
-    SenderID INT,
-    ReceiverID INT,
-    Status VARCHAR(50),
-    FOREIGN KEY (SenderID) REFERENCES Users(UserID),
-    FOREIGN KEY (ReceiverID) REFERENCES Users(UserID)
+invitations (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    sender_id INT > users.id,
+    receiver_id INT > users.id,
+    status VARCHAR(50) NOT NULL
+);
+
+-- Table des statistiques
+statistics (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    player_id INT > users.id,
+    wins INT,
+    losses INT
 );
 
 -- Table des connexions récentes
-CREATE TABLE RecentConnections (
-    ConnectionID INT PRIMARY KEY,
-    UserID INT,
-    LastConnection DATETIME,
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+recent_connections (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    player_id INT > users.id,
+    last_connection TIMESTAMP
 );
 
 -- Table des paramètres du jeu
-CREATE TABLE GameSettings (
-    SettingID INT PRIMARY KEY,
-    -- Ajoutez ici les paramètres spécifiques à la partie
+game_settings (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    max_players INT,
+    time_limit INT
 );
 
 -- Table des historiques
-CREATE TABLE GameHistory (
-    HistoryID INT PRIMARY KEY,
-    MatchID INT,
-    -- Ajoutez ici les événements de la partie
-    FOREIGN KEY (MatchID) REFERENCES Matches(MatchID)
+game_history (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    match_id INT > matches.id,
+    event_description VARCHAR(255)
+);
+
+
+-- Table des cartes
+cards (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    type VARCHAR(50),
+    attack INT,
+    defense INT,
+    cost INT
+);
+
+-- Table des decks
+decks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT > users.id,
+    name VARCHAR(255),
+    description TEXT
+);
+
+-- Table intermédiaire pour la relation entre decks et cartes (pour gérer les cartes dans un deck)
+deck_cards (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    deck_id INT > decks.id,
+    card_id INT > cards.id,
+    quantity INT
 );
